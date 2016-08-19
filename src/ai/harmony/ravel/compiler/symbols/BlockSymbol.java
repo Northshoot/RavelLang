fb27162e133ba995540a58a465981e5bcadf6ab4
Northshoot <laurynas.riliskis@gmail.com>
2016-08-19 12:21:34 -0700

    Added component symbols

---
+++ src/ai/harmony/ravel/compiler/symbols/BlockSymbol.java
@@ -0,0 +1,45 @@
+package ai.harmony.ravel.compiler.symbols;
+
+import ai.harmony.ravel.compiler.scopes.Scope;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+/**
+ * Created by lauril on 8/19/16.
+ */
+public class BlockSymbol extends Symbol implements Scope {
+    Map<String, Symbol> arguments = new LinkedHashMap<String, Symbol>();
+    Scope enclosingScope;
+
+    //models will take arguments
+    Map<String, Symbol> orderedArgs = new LinkedHashMap<String, Symbol>();
+
+    public BlockSymbol(String name, Symbol.Type mType, Scope currentScope) {
+        super(name, mType);
+        this.enclosingScope = enclosingScope;
+
+    }
+
+
+    public Symbol resolve(String name) {
+        Symbol s = arguments.get(name);
+        if ( s!=null ) return s;
+        // if not here, check any enclosing scope
+        if ( getEnclosingScope() != null ) {
+            return getEnclosingScope().resolve(name);
+        }
+        return null; // not found
+    }
+
+    public void define(Symbol sym) {
+        arguments.put(sym.name, sym);
+        sym.scope = this; // track the scope in each symbol
+    }
+
+    public Scope getEnclosingScope() { return enclosingScope; }
+    public String getScopeName() { return name; }
+
+    public String toString() { return "Model"+super.toString()+":"+arguments.values(); }
+} {
+}
